909a2872a6b17d88d86f885a700766bf3e632c1c
Giang Tran <Mac-Cafe@Mac-Cafes-MacBook-Pro.local>
2015-05-10 23:34:58 +0700

    Initial Commit

---
+++ GolfNation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GolfNation
+//
+//  Created by Giang Tran on 5/10/15.
+//  Copyright (c) 2015 Giang Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GolfNation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GolfNation
+//
+//  Created by Giang Tran on 5/10/15.
+//  Copyright (c) 2015 Giang Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
